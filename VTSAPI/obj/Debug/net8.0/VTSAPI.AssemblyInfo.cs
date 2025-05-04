327f7658d3d5d5008d2b434a12f945b8a0e894d2
KartikEkambe <110282851+KartikEkambe@users.noreply.github.com>
2025-05-04 16:59:07 +0530

    adding HomeController

--- VTSAPI/obj/Debug/net8.0/VTSAPI.AssemblyInfo.cs
+++ VTSAPI/obj/Debug/net8.0/VTSAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VTSAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53ebcd7f631fb0544e09c215b8ab337b090cf780")]
 [assembly: System.Reflection.AssemblyProductAttribute("VTSAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VTSAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
